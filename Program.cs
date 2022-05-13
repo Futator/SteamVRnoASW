6162c0710c9ee7ebcc19c10253de3327d01365c9
hegem <hegem@MATYAS-PC>
2022-05-13 13:46:08 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.IO;
+using Microsoft.Win32;
+
+namespace SteamVRnoASW
+{
+    public class SteamVRNoAsw
+    {
+        public static void ASWCommandFile()
+        {
+            StreamWriter sw = new StreamWriter(".\\asw_off.txt");
+            sw.WriteLine("server:asw.Off");
+            sw.WriteLine("exit");
+            sw.Close();
+        }
+        public static void GetCLIPath()
+        {
+            // second try at getting path but more complex and actually working
+            string oculusPath = String.Empty;
+            RegistryKey localKey = RegistryKey.OpenBaseKey(Microsoft.Win32.RegistryHive.LocalMachine, RegistryView.Registry64);
+            localKey = localKey.OpenSubKey(@"SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall\Oculus");
+            if (localKey != null)
+            {
+                oculusPath = localKey.GetValue("InstallLocation").ToString();
+            }
+            string cliPath = oculusPath + @"Support\oculus-diagnostics\OculusDebugToolCLI.exe";
+            Console.WriteLine(cliPath);
+            System.Diagnostics.Process.Start(cliPath, "/C -f asw_off.txt");
+        }
+        public static void Main(string[] args)
+        {
+            ASWCommandFile();
+            System.Threading.Thread.Sleep(2000);
+            GetCLIPath();
+            File.Delete(".\\asw_off.txt");
+            // System.Diagnostics.Process.Start("cmd.exe", "/C start steam://launch/250820");
+        }
+    }
+}
